d3518dcaf1862ef2aff0bb2cde0feec59601ca4c
hieu <letrunghieuxq@gmail.com>
2023-08-28 14:54:36 +0700

    #1 - h-Home,Shop

---
+++ HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appweb.help;
+using appweb.Models;
+
+
+namespace appweb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+      cart b=new cart();
+      
+        qlBhEntities db = new qlBhEntities();
+        home md = new home();
+       
+        public ActionResult Index()
+        {
+            if (Session["gmail"] == null)
+            {
+                
+             
+                ViewBag.tongSL = b.tongSL();
+
+            }
+            else
+            {
+                ViewBag.tongSL = b.tongSL(Session["gmail"].ToString());
+            }
+            md.sanPham = db.sanPham.ToList();
+            md.hsx=db.HSX.ToList();
+            return View(md);
+        }
+     
+
+
+    }
+}
---
+++ ShopController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appweb.Models;
+using System.Data;
+using System.IO;
+using System.Data.SqlClient;
+using appweb.Controllers;
+using PagedList;
+using appweb.help;
+using System.Web.WebPages;
+using System.Linq.Dynamic;
+
+namespace appweb.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        cart b = new cart();
+
+        public ActionResult Index(int? page, int? pageSize)
+        {
+            qlBhEntities db = new qlBhEntities();
+            List<sanPham> dsSP = db.sanPham.ToList();
+            if (page == null) page = 1;
+            if (pageSize == null) pageSize = 3;
+            ViewBag.pagesize = pageSize;
+            if (Session["gmail"] == null)
+            {
+
+
+                ViewBag.tongSL = b.tongSL();
+
+            }
+            else
+            {
+                ViewBag.tongSL = b.tongSL(Session["gmail"].ToString());
+            }
+
+            return View(dsSP.ToPagedList((int)page, (int)pageSize));
+        }
+        
+    }
+    }
